82ca4cd421a98c9843b5ee13214f07a4548c3708
Alberto Cardellini <6661715+ilcardella@users.noreply.github.com>
2022-03-05 18:15:09 +0000

    Fixed distortion effect implementation and updated unit test

--- extern/guitar_fx_lib/include/guitar_fx_lib/fx/distortion.hpp
+++ extern/guitar_fx_lib/include/guitar_fx_lib/fx/distortion.hpp
@@ -27,8 +27,8 @@ class Distortion : public Effect
         float threshold_min = 0.05f;
 
         // Higher values should reduce the threshold window and viceversa
-        float threshold =
-            std::max<float>(threshold_min, (signal_max - signal_min) * (1.0f - new_gain));
+        float threshold = std::max<float>(threshold_min, ((signal_max - signal_min) / 2) *
+                                                             (1.0f - new_gain));
         upper_bound = signal_mid + threshold;
         lower_bound = signal_mid - threshold;
     }
--- extern/guitar_fx_lib/tests/test_effect_distortion.cpp
+++ extern/guitar_fx_lib/tests/test_effect_distortion.cpp
@@ -32,33 +32,38 @@ TEST_F(TestEffectDistortion, testSetGain)
 TEST_F(TestEffectDistortion, testProcessValidSignal)
 {
     float output = 0.0f;
-    float threshold_min = 0.02f; // From effect implementation
-    float threshold_max = 1.0f;
-    float gain = 0.2f; // = 80%
+    const float threshold_min = 0.05f; // From effect implementation
 
-    float threshold =
-        std::max<float>(threshold_min, (threshold_max - threshold_min) * (1 - gain));
-    float upper_threshold = threshold;
-    float lower_threshold = -1 * threshold;
-
-    Distortion dist;
-    dist.set_gain(gain);
-
-    for (float input = -1.0f; input <= 1.0f; input += 0.1f)
+    for (float gain = 0.0f; gain <= 1.0f; gain += 0.1f)
     {
-        ASSERT_TRUE(dist.process(input, output));
+        // Expected thresholds, these depends on the fx implementation
+        const float threshold = std::max<float>(threshold_min, (1 - gain));
+        const float upper_threshold = threshold;
+        const float lower_threshold = -threshold;
 
-        if (input > upper_threshold)
+        Distortion dist;
+        dist.set_gain(gain);
+
+        for (float input = -1.0f; input <= 1.0f; input += 0.1f)
         {
-            ASSERT_EQ(output, upper_threshold);
-        }
-        else if (input < lower_threshold)
-        {
-            ASSERT_EQ(output, lower_threshold);
-        }
-        else
-        {
-            ASSERT_EQ(output, input);
+            ASSERT_TRUE(dist.process(input, output))
+                << "Failed with input " << input << " and gain " << gain;
+
+            if (input > upper_threshold)
+            {
+                ASSERT_EQ(output, upper_threshold)
+                    << "Failed with input " << input << " and gain " << gain;
+            }
+            else if (input < lower_threshold)
+            {
+                ASSERT_EQ(output, lower_threshold)
+                    << "Failed with input " << input << " and gain " << gain;
+            }
+            else
+            {
+                ASSERT_EQ(output, input)
+                    << "Failed with input " << input << " and gain " << gain;
+            }
         }
     }
 }
